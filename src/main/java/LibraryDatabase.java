61a3765be842017cfbbc9330548700a1238b78f4
Amiran Kurman <amiranfox@gmail.com>
2020-10-26 22:37:01 +0600

    added: postgres database full management

---
+++ src/main/java/LibraryDatabase.java
@@ -0,0 +1,50 @@
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class LibraryDatabase {
+    private static LibraryDatabase instance = new LibraryDatabase();
+
+    public static LibraryDatabase getInstance() {
+        return instance;
+    }
+
+    private LibraryDatabase() {}
+
+    private Connection getConnection()
+    {
+        Context initialContext;
+        Connection connection = null;
+        try
+        {
+            initialContext = new InitialContext();
+            Context envCtx = (Context)initialContext.lookup("java:comp/env");
+            DataSource ds = (DataSource)envCtx.lookup("jdbc/library");
+            connection = ds.getConnection();
+        }
+        catch (NamingException | SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    /*public LibraryDatabase create(LibraryDatabase todo ) {
+        String sql = "INSERT INTO TODO ( task) VALUES ( ?)";
+        try {
+            Connection connection = getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, todo.getTask());
+            ps.executeUpdate();
+            ps.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return todo;
+    }*/
+}
